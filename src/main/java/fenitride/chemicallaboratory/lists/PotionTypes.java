545e60f8843deb44d01a390f3ab1c06ff0b1dbdd
Doctor-Fe <sota.m8g1y3+git@gmail.com>
2023-06-21 17:34:11 +0900

    一部のPotionのインスタンスを直接取得するように変更

--- src/main/java/fenitride/chemicallaboratory/lists/PotionTypes.java
+++ src/main/java/fenitride/chemicallaboratory/lists/PotionTypes.java
@@ -1,6 +1,6 @@
 package fenitride.chemicallaboratory.lists;
 
-import net.minecraft.potion.Potion;
+import net.minecraft.init.MobEffects;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.potion.PotionType;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
@@ -9,7 +9,7 @@ public class PotionTypes {
 
 	public static final PotionType ANTIDOTE_1 = new PotionType("antidote", new PotionEffect(Potions.ANTIDOTE, 1200, 0, false, false)).setRegistryName("chemical_laboratory:antidote");
 	public static final PotionType PURIFICATION_1 = new PotionType("purification", new PotionEffect(Potions.PURIFICATION, 600, 0, false, true)).setRegistryName("chemical_laboratory:purification");
-	public static final PotionType STRONGEST_POISON = new PotionType("strongest_poison", new PotionEffect(Potion.getPotionFromResourceLocation("minecraft:poison"), 1200, 2)).setRegistryName("chemical_laboratory:strongest_poison");
+	public static final PotionType STRONGEST_POISON = new PotionType("strongest_poison", new PotionEffect(MobEffects.POISON, 1200, 2)).setRegistryName("chemical_laboratory:strongest_poison");
 
 	public static void register()
 	{
