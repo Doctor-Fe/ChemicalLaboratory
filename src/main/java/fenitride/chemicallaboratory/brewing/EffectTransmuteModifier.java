caf808bfdbdf5b9a15df80953ad6b4bea63353de
Doctor-Fe <sota.m8g1y3+git@gmail.com>
2025-01-05 21:55:58 +0900

    マージのミスを修正

--- src/main/java/fenitride/chemicallaboratory/brewing/EffectTransmuteModifier.java
+++ src/main/java/fenitride/chemicallaboratory/brewing/EffectTransmuteModifier.java
@@ -1,9 +1,6 @@
 package fenitride.chemicallaboratory.brewing;
 
 import java.util.HashMap;
-package fenitride.chemicallaboratory.brewing;
-
-import java.util.HashMap;
 
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
